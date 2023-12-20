a658d8110cc738141e2ff2358ffcc79172a23b7e
Dagmawi Elias <dagmawielias2@gmail.com>
2023-12-20 10:06:52 +0300

    fixed the double directory

--- stockminder/settings.py
+++ stockminder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#bwjj(dax_z$q^v4$9h$sh5y=vbr-$9^wlg2aumuz5-*tlst('
+SECRET_KEY = 'django-insecure-xe=03gpw&1a2cqdf%!9vl+)%mou9@kjpse7!5a0-l698fbtpdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
