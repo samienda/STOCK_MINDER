f7a12e8f2504626c5425be7f6c684dc641c9de20
Dagmawi Elias <dagmawielias2@gmail.com>
2024-01-05 15:26:17 +0300

    some model changes and added purchase to the admin page

--- stock/admin.py
+++ stock/admin.py
@@ -3,6 +3,9 @@ from . import models
 
 # Register your models here.
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['productname', 'user', 'product_type', 'supplier', 'property', 'quantity', 'price']
 admin.site.register(models.Property)
 
 admin.site.register(models.User)
@@ -10,3 +13,5 @@ admin.site.register(models.User)
 admin.site.register(models.ProductType)
 
 admin.site.register(models.Supplier)
+
+admin.site.register(models.Purchase)    
--- stock/models.py
+++ stock/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator #, MaxValueValidator
 
 # Create your models here.
 
@@ -9,84 +9,50 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-
-    def __str__(self) ->str:
-        return self.username
     
-    class Meta:
-         ordering = ['username']
+    def __str__(self) -> str:
+        return self.username
 
 
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
-
-    def __str__(self) ->str:
-        return self.name
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     
-    class Meta:
-         ordering = ['name']
-
-
-class Purchase(models.Model):
-    productlist = models.ForeignKey('product', on_delete=models.CASCADE, related_name='purchase_items')
-    quantity = models.IntegerField(validators=[MinValueValidator(1)])
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1.00)])
-    date = models.DateField(auto_now_add=True)
-
+    def __str__(self) -> str:
+        return self.name
 
 
 class Supplier(models.Model):
     name = models.CharField(max_length=255)
     contact_info = models.CharField(max_length=255)
-
-    def __str__(self) ->str:
-        return self.name
     
-    class Meta:
-         ordering = ['name']
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class Purchase(models.Model):
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1.00)])
+    date = models.DateField(auto_now_add=True)
+    productlist = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='productlist', null=True, blank=True)
+    
+    
+    def __str__(self) -> str:
+        return f'{self.date}'
+    
+
 
 
 
 class Property(models.Model): 
-    SMALL = 'S'
-    EXTRA_SMALL = 'XS'
-    MEDIUM = 'M'
-    LARGE = 'L'
-    EXTRA_LARGE = 'XL'
-    DOUBLE_EXTRA_LARGE = 'XXL'
-
-    SIZE_CHOICES = (
-        (SMALL, 'Small'),
-        (EXTRA_SMALL, 'Extra Small'),
-        (MEDIUM, 'Medium'),
-        (LARGE, 'Large'),
-        (EXTRA_LARGE, 'Extra Large'),
-        (DOUBLE_EXTRA_LARGE, 'Double Extra Large'),
-    )  
-
-    ADIDAS = 'A'
-    NIKE = 'N'
-    PUMA = 'P'
-    REEBOK = 'R'
-    JORDAN = 'J'
-    # we have to add a search fiel for brand and size
-    BRAND_CHOICES = (
-        (ADIDAS, 'Adidas'),
-        (NIKE, 'Nike'),
-        (PUMA, 'Puma'),
-        (REEBOK, 'Reebok'),
-        (JORDAN, 'Jordan'),
-    )
-
-    brand = models.CharField(max_length=1, choices = BRAND_CHOICES)
-    size = models.CharField(max_length=3, choices = SIZE_CHOICES)
-
-    def __str__(self) ->str:
-        return self.BRAND_CHOICES[self.brand] + ' ' + self.SIZE_CHOICES[self.size]
+    brand = models.CharField(max_length=25)
+    size = models.CharField(max_length=10)
     
-    class Meta:
-         ordering = ['brand', 'size']
+    def __str__(self):
+        return " BRAND - " + self.brand + " SIZE - " + self.size
 
 
 class Product(models.Model):
@@ -96,8 +62,8 @@ class Product(models.Model):
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1.00)])
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
-    product_type = models.ForeignKey(ProductType, on_delete=models.PROTECT)
-    Supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT)
+    product_type = models.ForeignKey(ProductType, on_delete=models.PROTECT, related_name='products')
+    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT)
     threshold = models.IntegerField(validators=[MinValueValidator(0)])
 
 
