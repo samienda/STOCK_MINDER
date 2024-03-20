3a3b24f6c4e6b34bd11d63e595423c3bc7ba3ab5
iamsamienda <samuelendale7373@gmail.com>
2024-03-20 12:39:04 +0300

    Update Django version, configure allowed hosts, and add Vercel deployment configuration

--- stockminder/wsgi.py
+++ stockminder/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockminder.settings')
 
 application = get_wsgi_application()
+app = application
